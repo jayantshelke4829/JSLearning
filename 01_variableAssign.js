71a2589bda7960f4afc6d8c996f090369cd256f0
Jayant Shelke <jayantshelke4829@gmail.com>
2023-07-19 21:44:40 +0530

    01_variableAssign.js 02_typeOf.js
    01_variableAssign.js 02_typeOf.js

---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+var myFullname;
+console.log("before initialization   :"+myFullname);
+var myFulltName;
+myFullname = "Jayant Shelke";
+console.log("after initialization    :"+myFullname)
+var myFulltName;
+myFullname = "Jayant Shilbhadra Shelke";
+console.log("after updation          :"+myFullname);
+var myLove;
+myLove ="I love only JS";
+console.log(myLove);
+var firstName;
+firstName ="Jayant";
+console.log("First Name              :"+firstName);
+var lastName;
+lastName ="Shelke";
+console.log("Last Name               :"+lastName);
+var age;
+age ="22";
+console.log("age                     :"+age);
+var address;
+address="Shegaon";
+console.log("address                 :"+address);
+var pincode;
+pincode="444203";
+console.log("pincode                 :"+pincode);
+var gender;
+gender="male";
+console.log("gender                  :"+gender)
+var isMarried = false;
+console.log("is married              :"+isMarried);
+
+
---
+++ 02_typeOf.js
@@ -0,0 +1,22 @@
+var bankName;
+console.log("before initialization  :"+bankName)
+bankName="SBI Bank";
+console.log("after initialization   :"+bankName);
+console.log("data type              :"+typeof bankName);
+var marks;
+marks ="90%"
+console.log("marks                  :"+marks)
+console.log("data type              :"+typeof marks)
+var isWorking;
+console.log("before initialization  :"+isWorking)
+isWorking=true
+console.log("after initialization   :"+isWorking)
+console.log("data type              :"+typeof isWorking)
+var totalCount="Hundread and Seven"
+console.log("value is               :"+totalCount)
+console.log("data type              :"+typeof totalCount)
+var totalCount="107"
+console.log("value is               :"+totalCount)
+console.log("data type              :"+typeof totalCount)
+
+
