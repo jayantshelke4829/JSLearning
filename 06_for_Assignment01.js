81def8e0d6a6c2ed6bf3ae0f4c9cf07a42ff4a10
Jayant Shelke <jayantshelke4829@gmail.com>
2023-07-31 21:21:09 +0530

    for and while loop
    for and while loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,30 @@
+
+console.log(`---------------------------`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+}
+console.log(`---------------------------`);
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+}
+console.log(`---------------------------`);
+for (let index = 1; index <30; index=index+2) {
+    console.log(index);
+}
+console.log(`---------------------------`);
+for (let index = 0; index <20; index=index+2) {
+    console.log(index);
+}
+console.log(`---------------------------`);
+
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+}
+console.log(`---------------------------`);
+for (let index = 10; index <=100; index=index+10 +s) {
+    console.log(index);
+}
+console.log(`---------------------------`);
+for (let index = 100; index >=10; index=index) {
+    console.log(index);
+}
