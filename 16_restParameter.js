3e111db558e40dbc68180867dcfe6dbd54a7881b
Jayant Shelke <jayantshelke4829@gmail.com>
2023-08-22 07:55:28 +0530

    updated 16_restParameter.js
    updated 16_restParameter.js

--- 16_restParameter.js
+++ 16_restParameter.js
@@ -1,11 +1,12 @@
 function add(num1, num2, ...num ){
+    let sum1 = num1 + num2
     let sum = 0
     for (let index = 0; index < num.length; index++) {
-        sum =sum + num[index]
-    }    num3 = num1+num2+sum
+        sum = sum + num[index]
+    }   let num3 = num1+num2+sum
 
     // console.log(`num1 ${num1},  num2 ${num2}`);
-    console.log(num3);
+    console.log(`sum of first two parameters is - ${sum1} and total sum of all parameters is - ${num3}`);
 }
 add(5, 6);
 add(9, 5, 6);
